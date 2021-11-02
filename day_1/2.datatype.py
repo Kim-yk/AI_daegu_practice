9aa2f9e17c222fd31681c489b6efcec4300d568c
Kim Yong Kwon <76460920+Kim-yk@users.noreply.github.com>
2021-11-02 18:49:18 +0900

    day_1

---
+++ day_1/2.datatype.py
@@ -0,0 +1,4 @@
+print(1+1)
+print('Hello '+'world')
+print(10*10)
+print('Hello'*10)
---
+++ day_1/3.numpy.py
@@ -0,0 +1,15 @@
+print(1+1)
+print(4-2)
+print(4*4)
+print(4/2)
+
+print(pow(3,2))
+
+import math
+print(math.sqrt(16))
+
+import random
+num = random.random()
+print(num)
+
+print(random.randrange(100,200))
